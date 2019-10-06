d68c4f72389146d50326f22fa0706114d48ce1a9
Jonathan Hanson <jonathan@jonathan-hanson.org>
2019-10-06 14:10:50 -0700

    cleaning molecule files

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,7 +3,8 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
 
 
 def test_docker_service_enabled(host):
